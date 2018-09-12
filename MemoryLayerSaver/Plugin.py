12604dcad0a1537222ad8ec8cf942f832b545735
Chris Crook <ccrook@linz.govt.nz>
2018-09-12 19:12:35 +1200

    Disable save memory layer warning - fixes #7

--- MemoryLayerSaver/Plugin.py
+++ MemoryLayerSaver/Plugin.py
@@ -9,11 +9,16 @@ import sys
 from .MemoryLayerSaver import MemoryLayerSaver
 from . import Resources
 
+
 class Plugin:
+
+    _MemoryWarningSetting="askToSaveMemoryLayers"
+    _MemorySavedSetting="MemoryLayerSaver/memoryLayerSaveSetting"
 
     def __init__( self, iface ):
         self._iface = iface
         self._saver = MemoryLayerSaver(iface)
+        self._saveSetting=True
 
     def initGui(self):
         self._saver.attachToProject()
@@ -22,7 +27,20 @@ class Plugin:
                                    self._iface.mainWindow())
         self._infoAction.triggered.connect(self._saver.showInfo)
         self._iface.addPluginToMenu("&Memory layer saver", self._infoAction)
+        # Save the current warning setting.
+        # Also save the value in a MLS plugin specific setting, in case QGIS
+        # crashes and saved setting is lost.
+        # MLS setting is removed  on unload, so existing value should be used if present.
+        settings=QgsSettings()
+        saveSetting=settings.value(self._MemoryWarningSetting, True, section=QgsSettings.App )
+        saveSetting=settings.value(self._MemorySavedSetting, saveSetting, section=QgsSettings.Plugins )
+        settings.setValue(self._MemoryWarningSetting, False, section=QgsSettings.App )
+        settings.setValue(self._MemorySavedSetting, saveSetting, section=QgsSettings.Plugins )
+        self._saveSetting
 
     def unload( self ):
         self._iface.removePluginMenu("&Memory layer saver",self._infoAction)
         self._saver.detachFromProject()
+        settings=QgsSettings()
+        settings.setValue(self._MemoryWarningSetting, self._saveSetting, section=QgsSettings.App )
+        settings.remove(self._MemorySavedSetting, section=QgsSettings.Plugins )
